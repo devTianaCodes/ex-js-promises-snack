04814a54f3cfaba3c422b230f809544ad4d5ff39
SwitchTia <oblasser.tatiana@gmail.com>
2026-04-08 12:54:16 +0200

    snack01 + bonus

--- Esercizi/Snack01.js
+++ Esercizi/Snack01.js
@@ -3,15 +3,15 @@
 // dal link https://dummyjson.com/posts/{id}
 
 function getPostTitle(id) {
-    return fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Errore nel recupero del post: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((post) => post.title);
-  }
+  return fetch(`https://dummyjson.com/posts/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Errore nel recupero del post: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((post) => post.title);
+}
 
 getPostTitle(4).then(console.log);
 
@@ -22,26 +22,26 @@ getPostTitle(4).then(console.log);
 // dalla chiamata https://dummyjson.com/users/{post.userId}.
 
 function getPost(id) {
-    return fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Errore nel recupero del post: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((post) => {
-        return fetch(`https://dummyjson.com/users/${post.userId}`)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`Errore nel recupero dell'autore: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then((user) => {
-            post.user = user;
-            return post;
-          });
-      });
-  }
+  return fetch(`https://dummyjson.com/posts/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Errore nel recupero del post: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((post) => {
+      return fetch(`https://dummyjson.com/users/${post.userId}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Errore nel recupero dell'autore: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((user) => {
+          post.user = user;
+          return post;
+        });
+    });
+}
 
-  getPost(4).then(console.log);
+getPost(4).then(console.log);
